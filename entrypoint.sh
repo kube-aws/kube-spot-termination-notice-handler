432760ac2c762cf32541758df139b4956fb3808a
Yusuke Kuoka <ykuoka@gmail.com>
2016-07-29 16:45:41 +0900

    First commit

---
+++ entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# How to test:
+#  NAMESPACE=default POD_NAME=kubesh-3976960141-b9b9t ./this_script
+
+echo 'This script polls the "EC2 Spot Instance Termination Notices" endpoint to gracefully stop and then reschedule all the pods running on this Kubernetes node, up to 2 minutes before the EC2 Spot Instance backing this node is terminated.'
+echo 'See https://aws.amazon.com/jp/blogs/aws/new-ec2-spot-instance-termination-notices/ for more information.'
+
+if [ "${NAMESPACE}" == "" ]; then
+  echo '[ERROR] Environment variable `NAMESPACE` has no value set. You must set it via PodSpec like described in http://stackoverflow.com/a/34418819' 1>&2
+  exit 1
+fi
+
+if [ "${POD_NAME}" == "" ]; then
+  echo '[ERROR] Environment variable `POD_NAME` has no value set. You must set it via PodSpec like described in http://stackoverflow.com/a/34418819' 1>&2
+  exit 1
+fi
+
+NODE_NAME=$(kubectl --namespace ${NAMESPACE} get pod ${POD_NAME} --output jsonpath="{.Spec.NodeName}")
+
+if [ "${NODE_NAME}" == "" ]; then
+  echo "[ERROR] Unable to fetch the name of the node running the pod \"${POD_NAME}\" in the namespace \"${NAMESPACE}\". Maybe a bug?: " 1>&2
+  exit 1
+fi
+
+echo "\`kubectl drain ${NODE_NAME}\` will be executed once a termination notice is made."
+
+POLL_INTERVAL=${POLL_INTERVAL:-5}
+
+NOTICE_URL=${NOTICE_URL:-http://169.254.169.254/latest/meta-data/spot/termination-time}
+
+echo "Polling ${NOTICE_URL} every ${POLL_INTERVAL} second(s)"
+
+# To whom it may concern: http://superuser.com/questions/590099/can-i-make-curl-fail-with-an-exitcode-different-than-0-if-the-http-status-code-i
+while http_status=$(curl -o /dev/null -w '%{http_code}' -sL ${NOTICE_URL}); [ ${http_status} -ne 200 ]; do
+  echo $(date): ${http_status}
+  sleep ${POLL_INTERVAL}
+done
+
+echo $(date): ${http_status}
+
+kubectl drain ${NODE_NAME}
